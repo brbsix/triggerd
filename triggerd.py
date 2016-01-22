e9f82247f216ead484e1bb71ce0d34d9ef1c054a
Six <brbsix@gmail.com>
2016-01-22 15:00:22 -0800

    Bump version to 0.6.0

--- triggerd.py
+++ triggerd.py
@@ -6,7 +6,7 @@
 import logging
 
 __program__ = 'triggerd'
-__version__ = '0.5.4'
+__version__ = '0.6.0'
 __description__ = 'Trigger an event or notification ' \
                   'upon the output of a command.'
 
